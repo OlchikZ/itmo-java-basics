fca2c7a3a2b689009f24e447d44a83a6f7217790
olchik <shyke@mail.ru>
2025-06-12 13:13:43 +0300

    HW2

---
+++ src/itmo/java/basics/hw2/Calculator.java
@@ -0,0 +1,62 @@
+package itmo.java.basics.hw2;
+
+public class Calculator {
+    private Double num1;
+    private Double num2;
+
+    public Calculator() {
+    }
+
+    // СУММА
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public long sum(long num1, long num2) {
+        return num1 + num2;
+    }
+
+    public double sum(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    // ВЫЧИТАНИЕ
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public long subtract(long num1, long num2) {
+        return num1 - num2;
+    }
+
+    public double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    // УМНОЖЕНИЕ
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public long multiply(long num1, long num2) {
+        return num1 * num2;
+    }
+
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    // ДЕЛЕНИЕ
+    public int divide(int num1, int num2) {
+        return num1 / num2; // предполагается, что b != 0
+    }
+
+    public long divide(long num1, long num2) {
+        return num1 / num2;
+    }
+
+    public double divide(double num1, double num2) {
+        return num1 / num2;
+    }
+
+}
